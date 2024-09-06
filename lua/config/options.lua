cedb8f0d3ee76dcd717428a4fa3186f06288b941
Vincent Vella <vincevella0@gmail.com>
2024-09-05 23:11:27 -0400

    feat: add treesitter and fix tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.cursorline = true
