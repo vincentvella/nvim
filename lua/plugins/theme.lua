c9af1134193a4df25170c319a971a3c7fba4a747
Vince Vella <vince@Vinces-M3-MacBook-Pro.local>
2024-10-07 03:11:01 -0400

    fix: make noice less noisy

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -23,4 +23,16 @@ return {
 			monokai.load()
 		end,
 	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			routes = {
+				{
+					filter = { event = "notify", find = "No information available" },
+					opts = { skip = true },
+				},
+			},
+		},
+	},
 }
